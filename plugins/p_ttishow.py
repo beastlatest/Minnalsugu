c57948a47423f72abd4c18a2516fd250cc2fb4cf
beastlatest <94529177+beastlatest@users.noreply.github.com>
2021-12-25 23:36:21 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('🫂 Hєℓρ', url=f"https://t.me/{temp.U_NAME}?start=help"),
+            InlineKeyboardButton('🔮 Cнαηηєℓ 🔮 ', url='https://t.me/MallusHubb')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
